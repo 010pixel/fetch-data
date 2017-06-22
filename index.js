69204786cb7f9f8b742f56d0c10b2a85cf560751
010 Pixel <rkgandhi@mibtt-rkgandhi1.users.sphinet.com.sg>
2017-06-22 15:35:51 +0800

    Added option to get data as JSON.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var server_port = process.env.PORT || 8080
+var server_port = process.env.PORT || 8080;
+server_port = 9000;
  
- var http = require('http');
+var http = require('http');
 var fetchUrl = require("fetch").fetchUrl;
 var url = require("url");
 
 var server = http.createServer(function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	res.writeHead(200);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
 	var url_parts = url.parse(req.url,true);
 	var google_sheet_id = url_parts.query.google_sheet_id;
@@ -18,18 +18,48 @@ var server = http.createServer(function(req, res) {
 	var load_type = url_parts.query.load_type;
 
 	if ( !google_sheet_id || !gid || !load_type ) {
+
 		res.write("Please pass necessary parameters.");
 		res.end();
+
 	} else {
-	    var feedUrl = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=' + google_sheet_id + '&single=true&gid=' + gid + '&output=' + load_type + '';
 
-	    // "https://docs.google.com/spreadsheets/d/1lgIw2Q-RfjdmYsDziAM8CZem3Gruy-YR_khMY6_Q4Qk/pub?gid=537679121&single=true&output=tsv"
+		var feedUrl = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=' + google_sheet_id + '&single=true&gid=' + gid + '&output=' + (load_type == "json" ? "csv" : load_type) + '';
 
-		// source file is iso-8859-15 but it is converted to utf-8 automatically
-		fetchUrl(feedUrl, function(error, meta, body){
-			res.write(body);
-			res.end();
-		});
+		if ( load_type == "tsv" ) {
+			res.setHeader('content-type', 'text/tsv');
+		} else if ( load_type == "csv" ) {
+			res.setHeader('content-type', 'text/csv');
+		} else if ( load_type == "json" ) {
+			res.setHeader('content-type', 'application/json');
+		}
+
+		// "https://docs.google.com/spreadsheets/d/1lgIw2Q-RfjdmYsDziAM8CZem3Gruy-YR_khMY6_Q4Qk/pub?gid=537679121&single=true&output=tsv"
+
+		if ( load_type == "json" ) {
+
+			var csvjson = require('csvjson');
+
+			fetchUrl(feedUrl, function(error, meta, body){
+				var result = csvjson.toObject(body.toString(), {});
+					result = JSON.stringify(result);
+
+				res.writeHead(200);
+				res.write(result);
+				res.end();
+			});
+
+		} else {
+
+			// source file is iso-8859-15 but it is converted to utf-8 automatically
+			fetchUrl(feedUrl, function(error, meta, body){
+				res.writeHead(200);
+				res.write(body);
+				res.end();
+			});
+
+		}
+
 	}
 });
 server.listen(server_port);
@@ -39,11 +69,11 @@ server.listen(server_port);
 // fetch.Promise = require('bluebird'); 
 // plain text or html 
 // fetch('https://github.com/')
-//     .then(function(res) {
-//         return res.text();
-//     }).then(function(body) {
-//         console.log(body);
-//     });
+//	 .then(function(res) {
+//		 return res.text();
+//	 }).then(function(body) {
+//		 console.log(body);
+//	 });
 
 
 
